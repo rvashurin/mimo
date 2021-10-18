1c69fb4edc89fb93a49e59cfc6b777bbfd86a83e
rvashurin <rvashurin@gmail.com>
2021-10-18 16:53:49 +0300

    Update run script to new structure

--- run_experiments.sh
+++ run_experiments.sh
@@ -14,7 +14,7 @@ do
     do
       dir="./runs/$ens_size/$depth/$width"
       mkdir -p $dir
-      python fmnist.py --output_dir=$dir --dataset=fashion_mnist --width=$width --per_core_batch_size=128 --train_epochs=250 --l2=3e-4 --checkpoint_interval=250 --use_gpu=True --ensemble_size=$ens_size --base_learning_rate=0.01 --depth=$depth
+      python custom/fmnist.py --output_dir=$dir --dataset=fashion_mnist --width=$width --per_core_batch_size=128 --train_epochs=250 --l2=3e-4 --checkpoint_interval=250 --use_gpu=True --ensemble_size=$ens_size --base_learning_rate=0.01 --depth=$depth
     done
   done
 done
